21c14f04f46e0ee14c44db990479a4ef47f25934
FujiiNoritsugu <quercus491@gmail.com>
2024-09-07 09:05:58 +0900

    20240907

--- test_speech.py
+++ test_speech.py
@@ -15,28 +15,34 @@ import google.generativeai as genai
 SAMPLERATE = 44100
 SPEAKER_ID = 0
 
-open_jtalk_dict_dir = './voicevox_core/open_jtalk_dic_utf_8-1.11'
+open_jtalk_dict_dir = "./voicevox_core/open_jtalk_dic_utf_8-1.11"
 acceleration_mode = AccelerationMode.AUTO
+
 
 def get_input_device_index():
     # pyaudioで利用可能なデバイスをリストアップ
     audio = pyaudio.PyAudio()
     return audio.get_device_count() - 1
 
+
 # ChatGPTの回答をAlexaへの質問に変換する関数
 def convert_to_alexa_question(chatgpt_response):
-    pattern = r'(?<=[。？！、])'
+    pattern = r"(?<=[。？！、])"
     sentences = re.split(pattern, chatgpt_response)
     # シンプルな変換ロジックの例
     alexa_question = "アレクサ, " + sentences[0]
 
     return alexa_question
 
+
 # ChatGPTの回答を受け取り、Alexaの質問を生成する
 def generate_alexa_question(chatgpt_response):
     global gemini_model
-    gemini_respose = gemini_model.generate_content(f"かっこの言葉をアレクサへの質問に変えて。「{chatgpt_response}」")
+    gemini_respose = gemini_model.generate_content(
+        f"かっこの言葉をアレクサへの質問に変えて。「{chatgpt_response}」"
+    )
     return gemini_respose.text
+
 
 def callback(in_data, frame_count, time_info, status):
     global sprec
@@ -45,7 +51,7 @@ def callback(in_data, frame_count, time_info, status):
 
     try:
         audiodata = speech_recognition.AudioData(in_data, SAMPLERATE, 2)
-        sprec_text = sprec.recognize_google(audiodata, language='ja-JP')
+        sprec_text = sprec.recognize_google(audiodata, language="ja-JP")
         print(sprec_text)
 
         completion = openai_client.chat.completions.create(
@@ -53,7 +59,7 @@ def callback(in_data, frame_count, time_info, status):
             messages=[
                 {
                     "role": "system",
-                    "content":"あなたはずんだもんです。",
+                    "content": "あなたはずんだもんです。",
                 },
                 {
                     "role": "user",
@@ -80,7 +86,8 @@ def callback(in_data, frame_count, time_info, status):
         print(f"Error occurred: {e}")  # これでOpenAIのエラーもキャッチ
     finally:
         return (None, pyaudio.paContinue)
-    
+
+
 def main():
     # ファイルを開く
     with open("../chat_gpt_api_key", "r") as file:
@@ -94,18 +101,18 @@ def main():
         # ファイルからデータを読み込む
         api_key = file.read()
 
-    genai.configure(api_key="AIzaSyC7KNIHLspFrlWSr7d3LWjL0twSESqv3Vk")
+    genai.configure(api_key=api_key)
     global gemini_model
-    gemini_model = genai.GenerativeModel('gemini-pro')
+    gemini_model = genai.GenerativeModel("gemini-pro")
 
     global core
     core = VoicevoxCore(
         acceleration_mode=acceleration_mode, open_jtalk_dict_dir=open_jtalk_dict_dir
     )
-    
+
     core.load_model(SPEAKER_ID)
 
-    global sprec 
+    global sprec
     sprec = speech_recognition.Recognizer()  # インスタンスを生成
     # Audio インスタンス取得
     audio = pyaudio.PyAudio()
@@ -115,21 +122,24 @@ def main():
     if input_device_index is None:
         print("利用可能なマイク入力デバイスがありません。")
         return
-    
-    stream = audio.open(format=pyaudio.paInt16,
-                        rate=SAMPLERATE,
-                        channels=1, 
-                        input_device_index=input_device_index,
-                        input=True, 
-                        frames_per_buffer=SAMPLERATE*5, # 5秒周期でコールバック
-                        stream_callback=callback)
+
+    stream = audio.open(
+        format=pyaudio.paInt16,
+        rate=SAMPLERATE,
+        channels=1,
+        input_device_index=input_device_index,
+        input=True,
+        frames_per_buffer=SAMPLERATE * 5,  # 5秒周期でコールバック
+        stream_callback=callback,
+    )
     stream.start_stream()
     while stream.is_active():
         time.sleep(0.1)
-    
+
     stream.stop_stream()
     stream.close()
     audio.terminate()
-    
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     main()
