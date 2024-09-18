6722bf1e49bc430e0ff2d590ffaafb7a1226d57e
FujiiNoritsugu <quercus491@gmail.com>
2024-09-19 05:55:52 +0900

    20240919

--- try_gemini.py
+++ try_gemini.py
@@ -1,6 +1,10 @@
 import google.generativeai as genai
 
-genai.configure(api_key="YOUR_API_KEY")
+with open("../gemini_api_key", "r") as file:
+    # ファイルからデータを読み込む
+    api_key = file.read()
+
+genai.configure(api_key=api_key)
 model = genai.GenerativeModel('gemini-pro')
 response = model.generate_content("かっこの言葉をアレクサへの質問に変えて。「おはようございます。なにかごようでしょうか？」")
 print(response.text)
